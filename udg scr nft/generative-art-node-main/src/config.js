e36f6989b27ed4598aecab7b07c322e0ed5ab852
unknown <pro.yohannes@gmail.com>
2022-11-12 17:22:25 +0100

    oeman

---
+++ udg scr nft/generative-art-node-main/src/config.js
@@ -0,0 +1,27 @@
+const layersOrder = [
+    { name: 'background', number: 19 },
+    { name: 'body', number: 16 },
+    { name: 'mouth', number: 5 },
+    { name: 'accessoire 2', number: 3 },
+    { name: 'eyes', number: 15 },
+    { name: 'masque', number: 3 },
+    { name: 'wear', number: 10 },
+    { name: 'hat', number: 37 },
+    { name: 'accessoire 1', number: 3 },
+];   
+
+const format = {
+    width: 230,
+    height: 230
+};
+
+const rarity = [
+    { key: "", val: "original" },
+    { key: "_r", val: "rare" },
+    { key: "_sr", val: "super rare" },
+];
+
+const defaultEdition = 10;
+
+module.exports = { layersOrder, format, rarity, defaultEdition };
+
